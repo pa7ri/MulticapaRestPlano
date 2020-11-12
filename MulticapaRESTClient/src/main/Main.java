7a4ce02f163c72be0a5fe6174d0624716388eb35
Patricia <patrybapa@gmail.com>
2020-11-12 02:30:09 +0100

    task: añadir interfaz 'bonita'

--- MulticapaRESTClient/src/main/Main.java
+++ MulticapaRESTClient/src/main/Main.java
@@ -1,127 +1,343 @@
 package main;
 
-import java.io.IOException;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import negocio.ciudad.DelegadoCiudad;
 
-public class Main extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class Main {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public static void main(String[] args) {		
+		createMainMenu();
+	}
+	
+	private static void createMainMenu() {
+		JFrame frame= new JFrame(); 
+        frame.setTitle("Consultor de ubicaciones");
+        
+        JButton buttonGet, buttonPost, buttonDelete, buttonPut;
+        buttonGet = new JButton("Buscar localización");
+        buttonPost = new JButton("Añadir localización");
+        buttonDelete = new JButton("Borrar localización");
+        buttonPut = new JButton("Actualizar localización");
+        
+        frame.add(buttonGet);
+        buttonGet.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		frame.dispose();
+        		createGetMenu();
+        	}
+        });
+        frame.add(buttonPost);
+        buttonPost.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		frame.dispose();
+        		createPostMenu();
+        	}
+        });
+        frame.add(buttonDelete);
+        buttonDelete.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		frame.dispose();
+        		createDeleteMenu();
+        	}
+        });
+        frame.add(buttonPut);
+        buttonPut.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		frame.dispose();
+        		createPutMenu();
+        	}
+        });
+        
+        frame.setLayout(new GridLayout(4,1));
+        
+        frame.setSize(400, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+	}
 
-		String evento = null;
-		String mensaje = null;
-		String url = "/jsp/salida/salida.jsp";
+	private static void createGetMenu() {
+		JFrame frame= new JFrame(); 
+        frame.setTitle("Encuentra tu localización por ID");
+        
+        JPanel content = new JPanel();
+        JLabel label = new JLabel();        
+        label.setText("Introduce ID :");
+        JTextField inputID = new JTextField("2",10);
+        
+        content.add(label);
+        content.add(inputID);
+        content.setLayout(new GridLayout(1,2));
+        content.setSize(400, 100);
+        
+        JPanel buttons = new JPanel(); 
+        JButton buttonSearch = new JButton("Buscar");
+        buttonSearch.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		String message = "Error en la lectura";
+				int alert = JOptionPane.ERROR_MESSAGE;
+				String res;
+        		
+        		try { 
+	        		int id = Integer.parseInt(inputID.getText());
+	    			res = DelegadoCiudad.getInstancia().ciudadGET(id);	
+	    			
+	    			if (res != null) {
+	    				message = res;
+	    				alert = JOptionPane.INFORMATION_MESSAGE;
+	    			} 
+				} catch ( NumberFormatException exc) {} finally {
+					JOptionPane.showMessageDialog(null, message, "Respuesta", alert);
+				}
+        	}
+        });
+        JButton buttonBack = new JButton("Atrás");
+        buttonBack.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		frame.dispose();
+        		createMainMenu();
+        	}
+        });
+
+        buttons.add(buttonBack);
+        buttons.add(buttonSearch);
+        buttons.setLayout(new GridLayout(1,2));
+        buttons.setSize(400, 100);
+        
+        
+        frame.setLayout(new BorderLayout());
+        frame.add(content, BorderLayout.NORTH);
+        frame.add(buttons, BorderLayout.SOUTH);
+        
+        frame.setSize(400, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+	}
+	
+	private static void createPostMenu() {
+		JFrame frame= new JFrame(); 
+        frame.setTitle("Añade una localización");
+        
+        JPanel content = new JPanel();
+        JLabel labelName = new JLabel("Introduce nombre :");
+        JTextField inputName = new JTextField("Madrid",10);
+        JLabel labelLong = new JLabel("Introduce longitud :");
+        JTextField inputLong = new JTextField("24",10);
+        JLabel labelLat = new JLabel("Introduce latitud :");
+        JTextField inputLat = new JTextField("72",10);
+        JLabel labelActive = new JLabel("Introduce activo :");
+        JTextField inputActive = new JTextField("1",10);
+
+        content.add(labelName);
+        content.add(inputName);
+        content.add(labelLong);
+        content.add(inputLong);
+        content.add(labelLat);
+        content.add(inputLat);
+        content.add(labelActive);
+        content.add(inputActive);
+        content.setLayout(new GridLayout(4,2));
+        content.setSize(400, 250);
+        
+        JPanel buttons = new JPanel(); 
+        JButton buttonUpdate = new JButton("Añadir");
+        buttonUpdate.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		String message = "Error en la creación";
+				int alert = JOptionPane.ERROR_MESSAGE;
+				int res;
+        		
+        		try {        			
+	        		String name =inputName.getText();
+	        		int longi = Integer.parseInt(inputLong.getText());
+	        		int lat = Integer.parseInt(inputLat.getText());
+	        		int active = Integer.parseInt(inputActive.getText());
+	    			res = Integer.parseInt(DelegadoCiudad.getInstancia().ciudadPOST(name, longi, lat, active));	
+	    			
+	    			if (res != 0) {
+	    				message = "Se ha creado la localidad con id " + res;
+	    				alert = JOptionPane.INFORMATION_MESSAGE;
+	    			} 
+    			} catch ( NumberFormatException exc) {} finally {
+					JOptionPane.showMessageDialog(null, message, "Respuesta", alert);
+				}
+        	}
+        });
+        JButton buttonBack = new JButton("Atrás");
+        buttonBack.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		frame.dispose();
+        		createMainMenu();
+        	}
+        });
+
+        buttons.add(buttonBack);
+        buttons.add(buttonUpdate);
+        buttons.setLayout(new GridLayout(1,2));
+        buttons.setSize(400, 100);
+        
+        
+        frame.setLayout(new BorderLayout());
+        frame.add(content, BorderLayout.NORTH);
+        frame.add(buttons, BorderLayout.SOUTH);
+        
+        frame.setSize(400, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+	}
+	
+	private static void createDeleteMenu() {
+		JFrame frame= new JFrame(); 
+        frame.setTitle("Borra una localización por ID");
+        
+        JPanel content = new JPanel();
+        JLabel label = new JLabel();        
+        label.setText("Introduce ID :");
+        JTextField inputID = new JTextField("2",10);
+        
+        content.add(label);
+        content.add(inputID);
+        content.setLayout(new GridLayout(1,2));
+        content.setSize(400, 100);
+        
+        JPanel buttons = new JPanel(); 
+        JButton buttonDelete = new JButton("Borrar");
+        buttonDelete.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		String message = "Error en la eliminación";
+				int alert = JOptionPane.ERROR_MESSAGE;
+				int res;
+        		
+        		try {
+	        		int id = Integer.parseInt(inputID.getText());
+	    			res = Integer.parseInt(DelegadoCiudad.getInstancia().ciudadDELETE(id));	
+	    			
+	    			if (res != 0) {
+	    				message = "Se ha eliminado la localidad con id " + id;
+	    				alert = JOptionPane.INFORMATION_MESSAGE;
+	    			} 
+    			} catch ( NumberFormatException exc) {} finally {
+					JOptionPane.showMessageDialog(null, message, "Respuesta", alert);
+				}
+        	}
+        });
+        JButton buttonBack = new JButton("Atrás");
+        buttonBack.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		frame.dispose();
+        		createMainMenu();
+        	}
+        });
+
+        buttons.add(buttonBack);
+        buttons.add(buttonDelete);
+        buttons.setLayout(new GridLayout(1,2));
+        buttons.setSize(400, 100);
+        
+        
+        frame.setLayout(new BorderLayout());
+        frame.add(content, BorderLayout.NORTH);
+        frame.add(buttons, BorderLayout.SOUTH);
+        
+        frame.setSize(400, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+	}
+	
+	private static void createPutMenu() {
+		JFrame frame= new JFrame(); 
+        frame.setTitle("Actualiza una localización");
+        
+        JPanel content = new JPanel();
+        JLabel labelID = new JLabel("Introduce ID :");
+        JTextField inputID = new JTextField("2",10);
+        JLabel labelName = new JLabel("Introduce nombre :");
+        JTextField inputName = new JTextField("Madrid",10);
+        JLabel labelLong = new JLabel("Introduce longitud :");
+        JTextField inputLong = new JTextField("24",10);
+        JLabel labelLat = new JLabel("Introduce latitud :");
+        JTextField inputLat = new JTextField("72",10);
+        JLabel labelActive = new JLabel("Introduce activo :");
+        JTextField inputActive = new JTextField("1",10);
+
+        content.add(labelID);
+        content.add(inputID);
+        content.add(labelName);
+        content.add(inputName);
+        content.add(labelLong);
+        content.add(inputLong);
+        content.add(labelLat);
+        content.add(inputLat);
+        content.add(labelActive);
+        content.add(inputActive);
+        content.setLayout(new GridLayout(5,2));
+        content.setSize(400, 250);
+        
+        JPanel buttons = new JPanel(); 
+        JButton buttonUpdate = new JButton("Actualizar");
+        buttonUpdate.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		String message = "Error en la actualización";
+				int alert = JOptionPane.ERROR_MESSAGE;
+				int res;
+				
+				try {
+	        		int id = Integer.parseInt(inputID.getText());
+	        		String name =inputName.getText();
+	        		int longi = Integer.parseInt(inputLong.getText());
+	        		int lat = Integer.parseInt(inputLat.getText());
+	        		int active = Integer.parseInt(inputActive.getText());
+	    			res = Integer.parseInt(DelegadoCiudad.getInstancia().ciudadPUT(id, name, longi, lat, active));	
+	    			
+	    			if (res != 0) {
+	    				message = "Se ha actualizado la localidad con id " + id;
+	    				alert = JOptionPane.INFORMATION_MESSAGE;
+	    			}
+				} catch ( NumberFormatException exc) {} finally {
+					JOptionPane.showMessageDialog(null, message, "Respuesta", alert);
+				}
+    			
+    			
+
+        	}
+        });
+        JButton buttonBack = new JButton("Atrás");
+        buttonBack.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		frame.dispose();
+        		createMainMenu();
+        	}
+        });
+
+        buttons.add(buttonBack);
+        buttons.add(buttonUpdate);
+        buttons.setLayout(new GridLayout(1,2));
+        buttons.setSize(400, 100);
+        
+        
+        frame.setLayout(new BorderLayout());
+        frame.add(content, BorderLayout.NORTH);
+        frame.add(buttons, BorderLayout.SOUTH);
+        
+        frame.setSize(400, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 		
-		evento = request.getParameter("event");
-
-		switch (evento) {
-		case "index": {
-			url = "/index.html";
-			break;
-		}
-		case "toCreate": {
-			url = "/jsp/localidad/create.jsp";
-			break;
-		}
-		case "toRead": {
-			url = "/jsp/localidad/read.jsp";
-			break;
-		}
-		case "toUpdate": {
-			url = "/jsp/localidad/update.jsp";
-			break;
-		}
-		case "toDelete": {
-			url = "/jsp/localidad/delete.jsp";
-			break;
-		}
-		case "create": {
-			String nombre = request.getParameter("nombre");
-			int longitud = Integer.parseInt((request.getParameter("longitud")));
-			int latitud = Integer.parseInt((request.getParameter("latitud")));
-			int activo = Integer.parseInt((request.getParameter("activo")));
-		
-			int res = Integer.parseInt(DelegadoCiudad.getInstancia().ciudadPOST(nombre, longitud, latitud, activo));
-			
-			if (res != 0) {
-				mensaje = "Se ha creado la localidad con id " + res;
-			} else {
-				mensaje = "Error en la creacion";
-			}
-
-			break;
-		}
-		case "read": {
-			int id = Integer.parseInt((request.getParameter("id")));
-			String res= DelegadoCiudad.getInstancia().ciudadGET(id);	
-			
-			if (res != null) {
-				mensaje = res;
-			} else {
-				mensaje = "Error en la lectura";
-			}
-
-			break;
-		}
-
-		case "update": {
-			int id = Integer.parseInt((request.getParameter("id")));
-			String nombre = request.getParameter("nombre");
-			int longitud = Integer.parseInt((request.getParameter("longitud")));
-			int latitud = Integer.parseInt((request.getParameter("latitud")));
-			int activo = Integer.parseInt((request.getParameter("activo")));
-
-			int res = Integer.parseInt(DelegadoCiudad.getInstancia().ciudadPUT(id, nombre, longitud, latitud, activo));
-			
-			if (res != 0) {
-				mensaje = "Se ha actualizado la localidad con id " + id;
-			} else {
-				mensaje = "Error en la actualizacion";
-			}
-
-			break;
-		}
-
-		case "delete": {
-			int id = Integer.parseInt((request.getParameter("id")));
-			
-			int res =Integer.parseInt(DelegadoCiudad.getInstancia().ciudadDELETE(id));
-			
-			if (res == 1) {
-				mensaje = "Se ha eliminado la localidad con id " + id;
-			} else {
-				mensaje = "Error en la eliminacion";
-			}
-
-			break;
-		}
-
-		default: {
-			url = "/index.html";
-			break;
-		}
-		}
-
-		request.getSession().setAttribute("mensaje", mensaje);
-
-		getServletContext().getRequestDispatcher(url).forward(request, response);
-
 	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doGet(request, response);
-	}
-
-	public static void main(String[] args) {
-		System.out.println("hola");
-	}
-
+	
 }
